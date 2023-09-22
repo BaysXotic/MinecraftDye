7b156b6d3162862bac5afdc004cff4976761da14
BaysXotic <145719670+BaysXotic@users.noreply.github.com>
2023-09-22 16:03:10 -0700

    Update EventListener.php

--- src/Terpz710/ShulkerDye/EventListener.php
+++ src/Terpz710/ShulkerDye/EventListener.php
@@ -7,7 +7,6 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\nbt\tag\ByteTag;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\tile\ShulkerBox;
-use pocketmine\block\Block;
 use pocketmine\player\Player;
 use pocketmine\item\LegacyStringToItemParser;
 use pocketmine\item\StringToItemParser;
@@ -23,18 +22,21 @@ class EventListener implements Listener {
             ? new StringToItemParser()
             : new LegacyStringToItemParser();
 
-        $parsedItem = $itemParser->parse($item->getName());
-
-        if ($parsedItem->getId() === $parsedItem::DYE) {
+        $parsedItem = $itemParser->parse($item->getCustomName());
+        if ($parsedItem->getId() === 351) {
             $dyeColor = $parsedItem->getDamage();
-            $tile = $block->getLevel()->getTile($block);
+            $level = $block->getLevel();
 
-            if ($tile instanceof ShulkerBox) {
-                $nbt = $tile->getNamedTag() ?? new CompoundTag("");
-                $nbt->setTag(new ByteTag("Color", $dyeColor));
-                $tile->setNamedTag($nbt);
-                $tile->saveNBT();
-                $player->sendMessage("Shulker Box color changed!");
+            if ($level->getBlockAt($block->x, $block->y, $block->z)->getId() === 219) { // Shulker Box ID is 219
+                $tile = $level->getTile($block);
+
+                if ($tile instanceof ShulkerBox) {
+                    $nbt = $tile->getNamedTag() ?? new CompoundTag("");
+                    $nbt->setTag(new ByteTag("Color", $dyeColor));
+                    $tile->setNamedTag($nbt);
+                    $tile->saveNBT();
+                    $player->sendMessage("Shulker Box color changed!");
+                }
             }
         }
     }
