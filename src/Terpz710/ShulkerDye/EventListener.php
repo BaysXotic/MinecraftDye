822e540bbc47c63279e7dcdd5748b8e648720bcc
BaysXotic <145719670+BaysXotic@users.noreply.github.com>
2023-09-22 16:04:57 -0700

    Update EventListener.php

--- src/Terpz710/ShulkerDye/EventListener.php
+++ src/Terpz710/ShulkerDye/EventListener.php
@@ -4,12 +4,12 @@ namespace Terpz710\ShulkerDye;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Dye;
 use pocketmine\nbt\tag\ByteTag;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\tile\ShulkerBox;
+use pocketmine\block\Block;
 use pocketmine\player\Player;
-use pocketmine\item\LegacyStringToItemParser;
-use pocketmine\item\StringToItemParser;
 
 class EventListener implements Listener {
 
@@ -18,25 +18,16 @@ class EventListener implements Listener {
         $block = $event->getBlock();
         $item = $player->getInventory()->getItemInHand();
 
-        $itemParser = class_exists(StringToItemParser::class)
-            ? new StringToItemParser()
-            : new LegacyStringToItemParser();
+        if ($item->getId() === Item::DYE) {
+            $dyeColor = $item->getDamage();
+            $tile = $block->getLevel()->getTile($block);
 
-        $parsedItem = $itemParser->parse($item->getCustomName());
-        if ($parsedItem->getId() === 351) {
-            $dyeColor = $parsedItem->getDamage();
-            $level = $block->getLevel();
-
-            if ($level->getBlockAt($block->x, $block->y, $block->z)->getId() === 219) { // Shulker Box ID is 219
-                $tile = $level->getTile($block);
-
-                if ($tile instanceof ShulkerBox) {
-                    $nbt = $tile->getNamedTag() ?? new CompoundTag("");
-                    $nbt->setTag(new ByteTag("Color", $dyeColor));
-                    $tile->setNamedTag($nbt);
-                    $tile->saveNBT();
-                    $player->sendMessage("Shulker Box color changed!");
-                }
+            if ($tile instanceof ShulkerBox) {
+                $nbt = $tile->getNamedTag() ?? new CompoundTag("");
+                $nbt->setTag(new ByteTag("Color", $dyeColor));
+                $tile->setNamedTag($nbt);
+                $tile->saveNBT();
+                $player->sendMessage("Shulker Box color changed!");
             }
         }
     }
