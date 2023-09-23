e0df1866ecb759ec7c514bc0641ddeed7eaea899
BaysXotic <145719670+BaysXotic@users.noreply.github.com>
2023-09-22 20:43:57 -0700

    Update EventListener.php

--- src/Terpz710/ShulkerDye/EventListener.php
+++ src/Terpz710/ShulkerDye/EventListener.php
@@ -7,9 +7,8 @@ namespace Terpz710\ShulkerDye;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\item\Dye;
-use pocketmine\item\Item;
-use pocketmine\item\ShulkerBox as ShulkerBoxItem;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
 use pocketmine\player\Player;
 
 class EventListener implements Listener {
@@ -21,19 +20,28 @@ class EventListener implements Listener {
         $item = $player->getInventory()->getItemInHand();
 
         if ($item instanceof Dye) {
-            
             $this->dyeDragData[$player->getName()] = $item->getColor();
         } elseif (isset($this->dyeDragData[$player->getName()])) {
-            
             $dyeColor = $this->dyeDragData[$player->getName()];
-            $shulkerBoxItem = new ShulkerBoxItem($dyeColor);
-
-            $player->getInventory()->removeItem(Item::get(Item::DYE, $dyeColor, 1));
-            
+            $dyeItem = new Dye($dyeColor);
+            $shulkerBoxItem = $this->createColoredShulkerBoxItem($dyeColor);
+            $this->transferCustomData($item, $shulkerBoxItem);
+            $player->getInventory()->remove($dyeItem);
             $player->getInventory()->setItemInHand($shulkerBoxItem);
             $player->sendMessage("Shulker Box color changed!");
 
             unset($this->dyeDragData[$player->getName()]);
         }
+    }
+
+    private function createColoredShulkerBoxItem(int $dyeColor): Dye {
+
+        $shulkerBoxItem = Dye::get(Item::SHULKER_BOX);
+        $nbt = new CompoundTag("", [
+            new StringTag(Dye::TAG_COLOR, $dyeColor)
+        ]);
+        $shulkerBoxItem->setNamedTag($nbt);
+
+        return $shulkerBoxItem;
     }
 }
