3ae0acfb6b1cb4c716dd50961fe3d7da411816d7
BaysXotic <145719670+BaysXotic@users.noreply.github.com>
2023-09-22 21:23:50 -0700

    Update EventListener.php

--- src/Terpz710/ShulkerDye/EventListener.php
+++ src/Terpz710/ShulkerDye/EventListener.php
@@ -7,8 +7,7 @@ namespace Terpz710\ShulkerDye;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\item\Dye;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\StringTag;
+use pocketmine\item\ShulkerBox;
 use pocketmine\player\Player;
 
 class EventListener implements Listener {
@@ -21,33 +20,16 @@ class EventListener implements Listener {
 
         if ($item instanceof Dye) {
             $this->dyeDragData[$player->getName()] = $item;
-        } elseif (isset($this->dyeDragData[$player->getName()])) {
+        } elseif (isset($this->dyeDragData[$player->getName()])) 
             $dyeItem = $this->dyeDragData[$player->getName()];
-            $dyeColor = $this->getDyeColor($dyeItem);
-            $shulkerBoxItem = $this->createColoredShulkerBoxItem($dyeColor);
-            $player->getInventory()->remove($dyeItem); 
+            $dyeColor = $dyeItem->getColor();
+            $shulkerBoxItem = new ShulkerBox($dyeColor);
+
+            $player->getInventory()->remove($dyeItem);
             $player->getInventory()->setItemInHand($shulkerBoxItem);
             $player->sendMessage("Shulker Box color changed!");
-
+        
             unset($this->dyeDragData[$player->getName()]);
         }
-    }
-
-    private function getDyeColor(Dye $dyeItem): int {
-        $nbt = $dyeItem->getNamedTag();
-        if ($nbt !== null && $nbt->hasTag(Dye::TAG_COLOR, StringTag::class)) {
-            return $nbt->getString(Dye::TAG_COLOR);
-        }
-        return Dye::COLOR_WHITE;
-    }
-
-    private function createColoredShulkerBoxItem(int $dyeColor): Dye {
-        $shulkerBoxItem = new Dye($dyeColor);
-        $nbt = new CompoundTag("", [
-            new StringTag(Dye::TAG_COLOR, $dyeColor)
-        ]);
-        $shulkerBoxItem->setNamedTag($nbt);
-
-        return $shulkerBoxItem;
     }
 }
